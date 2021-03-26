09eaa7d1cddcd083d1825986cee35ec438cb7351
Oliver Lorenz <mail@oliverlorenz.com>
2021-03-26 19:38:39 +0100

    fixes bug in script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 exportFiles() {
-  if [ -n "$EXPORT_JSON" ]: then
+  if [ -n "$EXPORT_JSON" ]; then
     tiled --export-map json "$1" "$(echo $1 | cut -f1 -d.).json"
   fi
-  if [ -n "$EXPORT_PNG" ]: then
+  if [ -n "$EXPORT_PNG" ]; then
     tmxrasterizer "$1" "$(echo $1 | cut -f1 -d.).png"
   fi
 }
