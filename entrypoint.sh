123a65bcd1114157ee7d0d5f4d856f5bc4ee866a
Oliver Lorenz <mail@oliverlorenz.com>
2021-03-27 17:50:45 +0100

    fixes bug in script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-exportFiles() {
+function exportFiles {
   if [ -n "$INPUT_EXPORT_JSON" ]; then
     echo "export json"
     tiled --export-map json "$1" "$(echo $1 | cut -f1 -d.).json"
