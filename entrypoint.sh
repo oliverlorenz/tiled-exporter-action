6a69b21d46a477d5dda68a1dd24f36379b5fd813
Oliver Lorenz <mail@oliverlorenz.com>
2021-03-27 18:19:56 +0100

    fixes bug in script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-TMX_FILE=/github/workspace/buero.tmx
-# xvfb-run find /github/workspace -name "$INPUT_PATTERN" -exec sh -c '
-
 if [ -n "$INPUT_EXPORT_JSON" ]; then
   echo "export json"
-  xvfb-run --auto-servernum tiled --export-map json "${TMX_FILE}" $(echo "${TMX_FILE}" | cut -f1 -d.).json
+  xvfb-run --auto-servernum tiled --export-map json "${INPUT_TMX_FILE}" $(echo "${INPUT_TMX_FILE}" | cut -f1 -d.).json
 fi
 if [ -n "$INPUT_EXPORT_PNG" ]; then
   echo "export png"
-  xvfb-run --auto-servernum tmxrasterizer "${TMX_FILE}" $(echo ${TMX_FILE} | cut -f1 -d.).png
+  xvfb-run --auto-servernum tmxrasterizer "${INPUT_TMX_FILE}" $(echo ${INPUT_TMX_FILE} | cut -f1 -d.).png
 fi
