d5e08854ca90f44c4004f142d5d474fb680951a3
Oliver Lorenz <mail@oliverlorenz.com>
2021-03-27 17:55:28 +0100

    fixes bug in script

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,18 @@ exportFiles () {
   fi
 }
 
-xvfb-run find . -name "$INPUT_PATTERN" -exec sh -c 'exportFiles "$0" {}' \;
+xvfb-run find . -name "$INPUT_PATTERN" -exec sh -c '
+
+  exportFiles () {
+    if [ -n "$INPUT_EXPORT_JSON" ]; then
+      echo "export json"
+      tiled --export-map json "$1" "$(echo $1 | cut -f1 -d.).json"
+    fi
+    if [ -n "$INPUT_EXPORT_PNG" ]; then
+      echo "export png"
+      tmxrasterizer "$1" "$(echo $1 | cut -f1 -d.).png"
+    fi
+  }
+
+  exportFiles "$0" {}
+' \;
